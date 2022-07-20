c2f3beda5fc956374e52a586403c3f5f2c039498
yamagame <yamagame@mac.com>
2022-07-21 00:24:16 +0900

    add bgcolor picker

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { GithubPicker, ColorResult } from "react-color";
 import "./App.css";
+
+const alpha = 0.8;
+
+const defaultColors = [
+  "#B80000",
+  "#DB3E00",
+  "#FCCB00",
+  "#008B02",
+  "#006B76",
+  "#1273DE",
+  "#004DCF",
+  "#5300EB",
+  "#EB9694",
+  "#FAD0C3",
+  "#FEF3BD",
+  "#C1E1C5",
+  "#BEDADC",
+  "#C4DEF6",
+  "#BED3F3",
+  "#D4C4FB",
+];
+
+function hexToRgb(hex: string) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+}
+
+const rgba = (hex: string, alpha: number) => {
+  const rgb = hexToRgb(hex);
+  if (rgb) {
+    return `rgba(${rgb.r},${rgb.g},${rgb.b},${alpha})`;
+  }
+  return hex;
+};
+
+const defaultPickerColors = defaultColors.map((color) => rgba(color, alpha));
 
 type Bookmark = {
   name: string;
   url: string;
+  backgroundColor?: string;
 };
 
 const loadBookmarks = () => {
@@ -14,6 +58,8 @@ const loadBookmarks = () => {
 
 function App() {
   const [name, setName] = React.useState("");
+  const [openPicker, setOpenPicker] = React.useState(false);
+  const [color, setColor] = React.useState<string>(defaultColors[13]);
   const [url, setUrl] = React.useState("");
   const [bookmarks, setBookmarks] = React.useState<Bookmark[]>(loadBookmarks() || []);
   const [selectedMarks, setSelectedMarks] = React.useState<string[]>([]);
@@ -35,6 +81,7 @@ function App() {
       if (selectedMarks.length > 0) {
         setSelectedMarks([]);
       }
+      setOpenPicker(false);
     };
     window.addEventListener("click", onClick);
     return () => {
@@ -46,21 +93,49 @@ function App() {
   const clickAddBookmark = (name: string, url: string) => {
     if (url === "") return;
     const title = name ? name : url;
-    setBookmarks((bookmarks) => [...bookmarks.filter((v) => v.url !== url), { name: title, url }]);
+    setBookmarks((bookmarks) => [
+      ...bookmarks.filter((v) => v.url !== url),
+      { name: title, url, backgroundColor: color },
+    ]);
     setSelectedMarks([]);
+  };
+
+  const toggleColorPicker = () => {
+    setOpenPicker((flag) => !flag);
+  };
+
+  const changeColor = (color: ColorResult) => {
+    setColor(color.hex);
+    setOpenPicker((flag) => !flag);
+    if (selectedMarks.length > 0) {
+      setBookmarks((bookmarks) => {
+        return bookmarks.map((v) => {
+          if (selectedMarks.indexOf(v.url) >= 0) {
+            return { ...v, backgroundColor: color.hex };
+          }
+          return v;
+        });
+      });
+    }
   };
 
   return (
     <>
       <div className="mark-top-header">
         <div className="mark-container" onClick={(e) => e.stopPropagation()}>
+          <div
+            className="mark-color"
+            style={{ backgroundColor: rgba(color, alpha) }}
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleColorPicker();
+            }}></div>
           <div className="mark-cell-small">
             <button
               className="mark-button"
               onClick={() => {
                 setName("");
-              }}
-            >
+              }}>
               Clear
             </button>
           </div>
@@ -83,8 +158,7 @@ function App() {
               className="mark-button"
               onClick={() => {
                 clickAddBookmark(name, url);
-              }}
-            >
+              }}>
               Add
             </button>
           </div>
@@ -104,15 +178,29 @@ function App() {
             />
           </div>
         </div>
+        {openPicker ? (
+          <div className="mark-color-picker" onClick={(e) => e.stopPropagation()}>
+            <GithubPicker color={color} onChange={changeColor} colors={defaultPickerColors} />
+          </div>
+        ) : null}
       </div>
       <div className="mark-body-container">
         {reversedBookmarks.map((v) => {
+          const selected = selectedMarks.indexOf(v.url) >= 0;
+          const color = v.backgroundColor || "azure";
+          const fontColor =
+            defaultColors.indexOf(color.toUpperCase()) > defaultColors.length / 2 ||
+            color === "azure"
+              ? "#444"
+              : "white";
           return (
             <div
               key={v.url}
-              className={
-                selectedMarks.indexOf(v.url) >= 0 ? "mark-card mark-selected" : "mark-card"
-              }
+              className="mark-card"
+              style={{
+                backgroundColor: rgba(color, alpha),
+                border: selected ? `solid 2px blue` : `solid 2px white`,
+              }}
               onClick={(e) => {
                 e.stopPropagation();
                 if (e.shiftKey) {
@@ -124,16 +212,18 @@ function App() {
                 }
                 setName(v.name);
                 setUrl(v.url);
-              }}
-            >
+                setColor(v.backgroundColor || "azure");
+              }}>
               <a
                 href={v.url}
                 rel="noopener noreferrer"
+                style={{
+                  color: fontColor,
+                }}
                 target="_blank"
                 onClick={() => {
                   clickAddBookmark(v.name, v.url);
-                }}
-              >
+                }}>
                 {v.name}
               </a>
             </div>
